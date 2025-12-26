6fe7de35acd5218772802a50118855a50ecd8287
Nianze TAO <39725660+Augus1999@users.noreply.github.com>
2025-12-26 11:26:14 +0900

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 import re
 from pathlib import Path
 from shutil import rmtree
-from setuptools import setup, find_packages
+from setuptools import setup  # , find_packages
 
 source_path = Path("bayesianflow_for_chem")
 
